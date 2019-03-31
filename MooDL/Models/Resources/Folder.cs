b5ed0eb516e5ceefd952b19650784161ce5508b2
reslario <44400244+reslario@users.noreply.github.com>
2019-03-31 19:23:20 +0200

    Create Folder.cs

---
+++ MooDL/Models/Resources/Folder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooDL.Models.Resources
+{
+    class Folder : Resource
+    {
+        public override string Extension => "";
+
+        public override string Type => "Folders";
+
+        public Resource[] Resources { get; set; }
+
+        public Folder(string url, string name) : base(url, name)
+        {
+        }
+    }
+}
